84764dce5ab723ce976634f242db37065386030d
AlexOdnov <alex.odnoralov@yandex.ru>
2020-09-20 16:59:59 +0300

    add first screen

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
 import mobileMenu from './mobileMenu';
+import { Swiper, Navigation } from 'swiper';
 
 mobileMenu();
+
+Swiper.use(Navigation);
+
+const swiperFirstScreen = new Swiper('.slider-promo', {
+  loop: true,
+  navigation: {
+    nextEl: '.slider-promo__button-next',
+    prevEl: '.slider-promo__button-prev',
+  },
+});
